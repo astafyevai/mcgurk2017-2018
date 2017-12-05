1789350a224625fe50cc34495106c9e8897ed209
agricolamz <agricolamz@gmail.com>
2017-12-05 10:57:11 +0300

    add plots about last year experiment

---
+++ data_from_old_experiment/experiment_2017.R
@@ -0,0 +1,25 @@
+library(tidyverse)
+library(ggridges)
+df <- read_tsv("experiment_2017.csv")
+
+df %>% 
+  count(sex, experiment) %>% 
+  full_join(df) %>% 
+  ggplot(aes(x = age, y = sex, fill = sex, label = n)) +
+  geom_density_ridges(show.legend = FALSE)+
+  theme_bw()+
+  facet_grid(~experiment, scales = "free")+
+  geom_text(aes(x = 10))+
+  labs(x = "", y = "")
+
+map <- read_csv("https://raw.githubusercontent.com/astafyevai/McGurk-effect/master/data/map.data.csv")
+
+library(lingtypology)
+map.feature(map$languages,
+            latitude = map$lat,
+            longitude = map$long,
+            control = F,
+            features = map$color,
+            color = c("black", "white"),
+            tile = "OpenStreetMap.BlackAndWhite",
+            legend = FALSE)
