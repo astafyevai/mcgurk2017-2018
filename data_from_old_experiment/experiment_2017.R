15bb6a5178dc3b9f95049fff96ba1e8e465c6ef1
agricolamz <agricolamz@gmail.com>
2017-12-06 21:00:41 +0300

    add one speaker

--- data_from_old_experiment/experiment_2017.R
+++ data_from_old_experiment/experiment_2017.R
@@ -3,13 +3,13 @@ library(ggridges)
 df <- read_tsv("experiment_2017.csv")
 
 df %>% 
-  count(sex, experiment) %>% 
+  count(sex, order, experiment) %>% 
   full_join(df) %>% 
   ggplot(aes(x = age, y = sex, fill = sex, label = n)) +
   geom_density_ridges(show.legend = FALSE)+
   theme_bw()+
-  facet_grid(~experiment, scales = "free")+
-  geom_text(aes(x = 10))+
+  facet_grid(experiment~order, scales = "free")+
+  geom_text(aes(x = 8))+
   labs(x = "", y = "")
 
 map <- read_csv("https://raw.githubusercontent.com/astafyevai/McGurk-effect/master/data/map.data.csv")
